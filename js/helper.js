e9b40c3bfa80c22ffb4c6887a24da0a34d1fdb45
Daniel Rosenbaum <daniel.rosenbaum10@gmail.com>
2017-11-15 20:35:04 -0800

    2.1 - Add course reviews

--- js/helper.js
+++ js/helper.js
@@ -3,177 +3,168 @@
 
 // constants used for http request 
 const URL = "https://aspc.pomona.edu/api/instructors/";
+const COURSE_URL = "https://aspc.pomona.edu/api/courses/";
 var list = true;
 var buttons = true;
 
 // set results
 var list_result = ""
-var grid_result = ""
+var course_result = ""
 var result = "";
 var	list_valid = true;
+var	course_valid = true;
 var loading = false;
 
 
 // function to format a professor with no results
 function noResults(input){
 	// display and update the results
-	list_result = ""
-	grid_result = ""
+	list_result = "<h1 class='section_header'>Professors</h1>"
+	document.getElementById('prof_title').disabled = true;
 
+	// display a picture of Cecil!
 	if (input == "Cecil Sagehen"){
-		return result = "<img style='margin-top: 10px; margin-left: 100px;' src='/images/cecil.jpg'>";
+		return result = "<img style='margin-top: 20px; margin-left: 120px;' src='/images/cecil.jpg'>";
 	}
-	return result = "<h6>No professors named: " + input + "</h6>";
+	return list_result += "<h6 style='margin-left: 15px;'>No professors named: " + input + "</h6>";
 
+}
+
+// function to format a professor with no results
+function noCourseResults(input){
+	// display and update the results
+	course_result = "<hr><hr><h1 class='section_header'>Courses</h1>"
+	document.getElementById('course_title').disabled = true;
+
+	// return nothing if Cecil Sagehen as this case is handled by noResults()
+	if (input == "Cecil Sagehen"){
+		return ""
+	}
+	return course_result += "<h6 style='margin-left: 15px;'>No courses named: " + input + "</h6>";
 }
 
 // function that formats all results
 // creates both a grid and list to make toggling between the two easier
 function viewResults(hash){
-	// reset the grid and list results
-		grid_result = "<table id='result_table' style='width: 785px; margin-top: 7px;'><thead><tr>" +
-			"<th class='name'>Professor</th>" +
-			"<th class='rating'>Rating</th>" +
-			"<th class='useful_rating'>Usefulness</th>" + 
-			"<th class='engagement_rating'>Engagement</th>" +
-			"<th class='difficulty_rating'>Difficulty</th>" +
-			"<th class='competency_rating'>Competency</th>" + 
-			"<th class='lecturing_rating'>Lecturing</th>" + 
-			"<th class='enthusiasm_rating'>Enthusiasm</th>" +
-			"<th class='approachable_rating'>Approachability</th>" +
-			"<th class='inclusivity_rating'>Inclusivity</th></tr></thead>" +
-			"<tbody><tr>";
+	// reset the list results
+	list_result = "<h1 class='section_header'>Professors</h1>"
+	document.getElementById('prof_title').disabled = false;
 
-		list_result = ""
-		
-		// go through all results returned by the query
-		hash.forEach(function (review) {
-			for (item in review){
 
-				if (item == "name") {
-					// professor name
-					list_result += "<span><hr><h4 class='prof_name'>" + review[item] + "</h4>";
-					
-					// information icon that takes you to the professor reviews link on ASPC site
-					url = "https://aspc.pomona.edu/courses/browse/instructor/" + review['id'] + "/"
-					list_result += "<a href='" + url + "' target='_blank'><i class='material-icons' style='font-size: 15px; float: right; margin-top: 10px; margin-right: 10px;'>&#xe88f;</i></a></span><div align='right'>"
-					
-					// add professor to grid and make their name clickable (to take you to the ASPC link)
-					grid_result += "<td class='" + item + "'><a id='grid_name' href='" + url + "' target='blank'>" + review[item] + "</a></td>";
+	// go through all results returned by the query
+	hash.forEach(function (review) {
+		for (item in review){
 
+			// print the name of the professor and the icon link to the ASPC reviews page
+			if (item == "name") {
+				// professor name
+				list_result += "<span><hr><h4 class='prof_name'>" + review[item] + "</h4>";
+				
+				// information icon that takes you to the professor reviews link on ASPC site
+				url = "https://aspc.pomona.edu/courses/browse/instructor/" + review['id'] + "/"
+				list_result += "<a href='" + url + "' target='_blank'><i class='material-icons' style='font-size: 15px; float: right; margin-top: 10px; margin-right: 10px;'>&#xe88f;</i></a></span><div align='right'>"
+			}
+			// update the results based on all the fields
+			else if (item != "id"){
+				// get the rating and the field title
+				var val = parseFloat(review[item]).toFixed(2);
+				var title = item.charAt(0).toUpperCase() + item.split("_")[0].slice(1);
+
+				// display no rating if there are no reviews 
+				// display an empty progress bar for incomplete reviews
+				if (isNaN(val)){
+
+					if (item == 'rating'){
+						list_result += "<p id='no_reviews'>There are no reviews yet!</p>";
+						list_valid = false;
+					}
+
+					if (list_valid){
+						progress = "<progress value='" + val + "' max='5'></progress>";				
+						list_result += "<p>" + title + " " + progress + " N/A</p>";
+					}
 
 				}
-				else if (item != "id"){
-					var val = parseFloat(review[item]).toFixed(2);
-					var title = item.charAt(0).toUpperCase() + item.split("_")[0].slice(1);
-
-
-					if (isNaN(val)){
-
-						grid_result += "<td class='" + item + "'>--</td>";
-
-						if (item == 'rating'){
-							list_result += "<p id='no_reviews'>There are no reviews yet!</p>";
-							list_valid = false;
-						}
-
-						if (list_valid){
-							progress = "<progress value='" + val + "' max='5'></progress>";				
-							list_result += "<p>" + title + " " + progress + " N/A</p>";
-						}
-
-					}
-					else {								
-						progress = "<progress value='" + val + "' max='5'></progress>";				
-						list_result += "<p>" + title + " " + progress + " " + val + "</p>";
-						grid_result += "<td class='" + item + "'>" + val + "</td>";
-					}
+				// display a progress bar with the value and the title
+				else {								
+					progress = "<progress value='" + val + "' max='5'></progress>";				
+					list_result += "<p>" + title + " " + progress + " " + val + "</p>";
 				}
 			}
+		}
 
-			// update the grid by closing a row and openning a new one
-			grid_result += "</tr><tr>";
-			list_result += "</div>"	
+		// close the div
+		list_result += "</div>"	
 
-			// reset the list to have valid reviews for new professor
-			list_valid = true;
+		// reset the list to have valid reviews for new professor
+		list_valid = true;
 
-		})
+	})
 
-		// end the grid 
-		grid_result += "</tbody></table>";
-
-		// based on selection - display the results accordingly
-		return result = list ? list_result : grid_result
+	// based on selection - display the results accordingly
+	return list_result
 }
 
 
-// function used to sort the table by column header
-function sortTable(n) {
-	var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
 
-	table = document.getElementById("result_table");
+// function that formats all results
+// creates both a grid and list to make toggling between the two easier
+function viewCourseResults(hash){
+	// reset the list results
+	course_result = "<hr><hr><h1 class='section_header'>Courses</h1>"
+	document.getElementById('course_title').disabled = false;
 
-	switching = true;
+	
+	// go through all results returned by the query
+	hash.forEach(function (review) {
+		for (item in review){
 
-	//Set the sorting direction
-	// should be asc for prof name and desc for all other headers
-	dir = n == 0 ? "asc" : "desc"
+			if (item == "name") {
+				// professor name
+				course_result += "<span><hr><h4 class='prof_name'>" + review[item] + "</h4>";
+				// information icon that takes you to the professor reviews link on ASPC site
+				url = "https://aspc.pomona.edu/courses/browse/course/" + review['code'].replace(/\s+/g, '-') + "/"
+				course_result += "<a href='" + url + "' target='_blank'><i class='material-icons' style='font-size: 15px; float: right; margin-top: 10px; margin-right: 10px;'>&#xe88f;</i></a></span>"
+				course_result += "<br><h6 class='class_code'>" + review["code"] + "</h6><div align='right'>";
 
-	/*Make a loop that will continue until
-	no switching has been done:*/
-	while (switching) {
-	//start by saying: no switching is done:
-		switching = false;
-		rows = table.getElementsByTagName("tr");
+			}
+			// make sure item is a valid rating item and not extra information
+			else if (item != "id" && item != "department" && item != "number" && item != "code"){
+				// get value and title
+				var val = parseFloat(review[item]).toFixed(2);
+				var title = item.charAt(0).toUpperCase() + item.split("_")[0].slice(1);
 
-		for (i = 1; i < (rows.length - 2); i++) {
-			//start by saying there should be no switching:
-			shouldSwitch = false;
-			/*Get the two elements you want to compare,
-			one from current row and one from the next:*/
-			if (n > 0){
-				x = rows[i].getElementsByTagName("td")[n].innerHTML.toLowerCase();
-		      	y = rows[i + 1].getElementsByTagName("td")[n].innerHTML.toLowerCase();
-	      	}
-	      	else {
-	      		x = rows[i].getElementsByTagName("td")[n].getElementsByTagName("a")[0].innerHTML
-	      		y = rows[i + 1].getElementsByTagName("td")[n].getElementsByTagName("a")[0].innerHTML
-	      	}
-			/*check if the two rows should switch place,
-			based on the direction, asc or desc:*/
-			if (dir == "asc") {
-				if (x > y) {
-					//if so, mark as a switch and break the loop:
-					shouldSwitch= true;
-					break;
+
+				if (isNaN(val)){
+					// either print out that there are no reviews or
+					// print an incomplete progress bar
+					if (item == 'rating'){
+						course_result += "<p id='no_reviews'>There are no reviews yet!</p>";
+						course_valid = false;
+					}
+
+					if (course_valid){
+						progress = "<progress value='" + val + "' max='5'></progress>";				
+						course_result += "<p>" + title + " " + progress + " N/A</p>";
+					}
+
 				}
-			} else if (dir == "desc") {
-				if (x < y) {
-					//if so, mark as a switch and break the loop:
-					shouldSwitch= true;
-					break;
+				// display the progress bar with the value and the title
+				else {								
+					progress = "<progress value='" + val + "' max='5'></progress>";				
+					course_result += "<p>" + title + " " + progress + " " + val + "</p>";
 				}
 			}
 		}
-		if (shouldSwitch) {
-			/*If a switch has been marked, make the switch
-			and mark that a switch has been done:*/
-			rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
-			switching = true;
-			//Each time a switch is done, increase this count by 1:
-			switchcount ++; 
-		} else {
-			/*If no switching has been done AND the direction is "asc",
-			set the direction to "desc" and run the while loop again.*/
-			if (switchcount == 0 && dir == "desc") {
-				dir = "asc";
-				switching = true;
-			}
-			else if (switchcount == 0 && dir == "asc") {
-				dir = "desc";
-				switching = true;
-			}
-		}
-	}
+
+		// close the div
+		course_result += "</div>"	
+
+		// reset the list to have valid reviews for new professor
+		course_valid = true;
+
+	})
+
+	// based on selection - display the results accordingly
+	return course_result
 }
--- js/popup.js
+++ js/popup.js
@@ -9,39 +9,44 @@ function init() {
 	// add listener for enter key and to allow user to press enter
 	document.getElementById('input').addEventListener('keypress', function (e) {
     	var key = e.which || e.keyCode;
-    	if (key === 13) { // 13 is enter
+    	 // 13 is enter
+    	if (key === 13) {
     		makeQuery();
-    		addButtons()
     	}
 	});     
 
 }
 
-// add buttons to the display
-function addButtons(){
-	document.getElementById("buttons_div").style.display = 'block'
-	addButtonListeners()
-
-	// addButtons shall only do something the first time it is called - so it does not continually add buttons
-	addButtons = function(){};
-}
-
 // makes the http request to get information 
 function makeQuery() {
 
+	// display the buttons
+    addTitles()	
+
+	// scroll back to the top of the results on each query
+	$('html, body').animate({
+	        scrollTop: $("#print_result").offset().top - 48
+	    }, 1000);
+
+	// de-select both buttons
+	$("#course_title").removeClass('selected_header')
+	$("#prof_title").removeClass('selected_header')
 	loading = true;
 
+	// add the loading spinner while the query is being made
 	loading_spinner = "<div class='loader'>Loading...</div>"
 	document.getElementById("print_result").innerHTML = loading_spinner;
 	
 	// get the input from the text field
 	var input = document.getElementById('input').value;
-
-	// create request and request url
+ 
+	// create request and request url for both professor and course reviews
 	var xmlHttp = new XMLHttpRequest();
-	var request = URL + input + TOKEN
+	var xmlHttpCourse = new XMLHttpRequest();
+	var request = URL + input + TOKEN;
+	var course_request = COURSE_URL + input + TOKEN;
 
-	// function that handles any state changes
+	// function that handles any state changes for the Professor query
 	xmlHttp.onreadystatechange = function() {
     	// success
     	if (xmlHttp.readyState==4 && xmlHttp.status==200){
@@ -58,23 +63,46 @@ function makeQuery() {
     	}
 	}
 
-	// open the connection
+	// function that handles any state changes for the Courses query
+	xmlHttpCourse.onreadystatechange = function() {
+    	// success
+    	if (xmlHttpCourse.readyState==4 && xmlHttpCourse.status==200){
+        	var msg = xmlHttpCourse.responseText;
+        	course_format(msg, input)
+    	}
+    	// 404 Error
+    	else if (xmlHttpCourse.readyState==4 && xmlHttpCourse.status==404){
+    		document.getElementById("print_result").innerHTML = "<h6>That page couldn't be found - please try again.</h6>"
+    	}
+    	// 502 Error
+    	else if (xmlHttpCourse.readyState==4 && xmlHttpCourse.status==502){
+    		document.getElementById("print_result").innerHTML = "<h6>The ASPC Site may be down. Sorry for the inconvienance - please try again.</h6>"
+    	}
+	}
+
+	// open the connection for both requests
 	xmlHttp.open("GET", request, true);
+	xmlHttpCourse.open("GET", course_request, true);
 
 	// set timeout to 7 seconds and function that handles timeouts
 	xmlHttp.timeout = 7000;
+	xmlHttpCourse.timeout = 7000;
 	xmlHttp.ontimeout = function(){
 		document.getElementById("print_result").innerHTML = "<h6>This is taking too long - please try again.</h6>"
+	}
+	xmlHttpCourse.ontimeout = function(){
+		document.getElementById("print_result_courses").innerHTML = "<h6>This is taking too long - please try again.</h6>"
 	}
 
 	// catch all other errors 
 	xmlHttp.onerror = function(){
 		document.getElementById("print_result").innerHTML = "<h6>There seems to be an error - please try again.</h6>"
 	}
-
-	// respond with null
+	xmlHttpCourse.onerror = function(){
+		document.getElementById("print_result_courses").innerHTML = "<h6>There seems to be an error - please try again.</h6>"
+	}
 	xmlHttp.send(null);
-
+	xmlHttpCourse.send(null);
 }
 
 
@@ -84,73 +112,54 @@ function format(responses, input){
 	// parse string return to json hash 
 	var hash = JSON.parse(responses)
 
-	result = hash.length == 0 ? noResults(input) : viewResults(hash);
+	// depending on the return json display results or no results
+	result = hash.length === 0 ? noResults(input) : viewResults(hash);
 
 	// display results to html for all to see!
 	document.getElementById("print_result").innerHTML = result;
 	document.getElementById("footer").style.display = 'block';
 
+	loading = false;
+}
 
-	if (!list){
-		for (index = 0; index < 10; index++){
-			addColumnListeners(index);
-		}
-	}
+// get the response then format and print to html 
+function course_format(responses, input){
+	// parse string return to json hash 
+	var hash = JSON.parse(responses)
+
+	// depending on the return json display results or no results
+	course_result = hash.length === 0 ? noCourseResults(input) : viewCourseResults(hash);
+
+	// display results to html for all to see!
+	document.getElementById("print_result_courses").innerHTML = course_result;
+	document.getElementById("footer").style.display = 'block';
 
 	loading = false;
-
 }
 
+// function that adds the buttons to the page
+function addTitles(){
+	document.getElementById("titles").innerHTML = "<button id='prof_title' class='title_header'>Professors</button><br><button id='course_title' class='title_header'>Courses</button>"
+	
+	// add functionality on click to scroll to the desired section
+	// also update which button is selected and unselected
+	$("#prof_title").click(function() {
+    	$('html, body').animate({
+        	scrollTop: $("#print_result").offset().top - 48
+    	}, 1000);
+    	$("#prof_title").addClass('selected_header');
+    	$("#course_title").removeClass('selected_header');
 
-// switch between either list or grid view depending on click
-function changeFormat(){
-
-	if (!loading){
-		if (list_result != "" && list){
-			result = list_result;
-			document.getElementById("print_result").innerHTML = result;
-		}
-		else if (grid_result != "" && !list){
-			result = grid_result;
-			document.getElementById("print_result").innerHTML = result;
-
-			// add listeners for each column header
-			for (var index = 0; index < 10; index++){
-				addColumnListeners(index);
-			}
-		}
-	}
-}
-
-// add a listener for each column based on id
-function addColumnListeners(index){
-	var hdrs = document.getElementsByTagName('th')
-
-	hdrs[index].addEventListener('click', function () {
-		sortTable(index)
 	});
-}
+	$("#course_title").click(function() {
+    	$('html, body').animate({
+        	scrollTop: $("#print_result_courses").offset().top - 40
+        }, 1000);
+        $("#course_title").addClass('selected_header');
+        $("#prof_title").removeClass('selected_header');
 
+	});
 
-function addButtonListeners(){
-	//add listener for type of format (list or grid)
-	document.getElementById('list_button').addEventListener('click', function () {
-		list = true;
-
-		// update button display
-		document.getElementById('list_button').disabled = true;
-		document.getElementById('grid_button').disabled = false;
-
-		changeFormat()
-	}); 
-
-	document.getElementById('grid_button').addEventListener('click', function () {
-		list = false;
-
-		// update button display
-		document.getElementById('grid_button').disabled = true;
-		document.getElementById('list_button').disabled = false;
-
-		changeFormat()
-	}); 
+	// addButtons shall only do something the first time it is called - so it does not continually add buttons
+	addTitles = function(){};
 }
